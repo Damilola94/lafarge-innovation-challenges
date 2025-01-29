8dfc53d1f8ce87b62bcdee237a63e17d256f173c
Damilola Ogunboyejo <demilademichael18@gmail.com>
2025-01-29 22:43:54 +0100

    feat:(completed the api call for submission)

--- src/components/challenges/challenges-1.jsx
+++ src/components/challenges/challenges-1.jsx
@@ -72,7 +72,7 @@ export function Challenge() {
   const [scenarioId, setScenarioId] = useState(2);
   const [selectedTeam, setSelectedTeam] = useState("Select Team");
 
-  console.log("Man");
+  console.log("Man/....");
   
   const UNIT_PRICES = {
     clinker: 50,
